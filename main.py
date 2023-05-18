11ac680484d75b8b991724a052fa136cbcff267a
b-chau23 <bowiechau2301@gmai.com>
2023-05-18 01:18:01 -0400

    Comment clean up

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-from dotenv import load_dotenv
 import os
-import discord
-from discord.ext import commands
-from random import randint
 import time
-
+import discord
+from random import randint
+from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv('.env')
 
 TOKEN: str = os.getenv('TOKEN')
 GUILD: str = os.getenv('GUILD')
 
-
 intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
+# Messages terminal to confirm successful bot connection
 @bot.event
 async def on_ready():
     for guild in bot.guilds:
@@ -48,12 +46,15 @@ async def mention_ping(ctx, member: discord.Member):  # REEEEEEEEEEEEEEEEEEEEEEE
     await ctx.send(f"REEEEEEEEEEE {member.mention}")
 
 
+# Error messages
 @bot.event
-async def on_command_error(ctx, error):  # Fails Role Check
+async def on_command_error(ctx, error):
+    # Role check failiure
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
-
-    elif isinstance(error, commands.errors.CommandOnCooldown):  # Lmao get fucked dumbass / sends over-use message
+    # Command on cooldown error
+    elif isinstance(error, commands.errors.CommandOnCooldown):
+        # Create a random chance to send a 'unique' error message
         slugma = randint(1, 10)
         if slugma == 1:
             await ctx.send("slugma balls bitch", file=discord.File('slugma.jpg'))
@@ -61,75 +62,72 @@ async def on_command_error(ctx, error):  # Fails Role Check
             await ctx.send('Command is on Cooldown.')
 
 
-# sends user to specified channel (in this case "hell")
+# Removes targetted user from their current channel and moves them to a new one
 @bot.command(name="banish")
 @commands.cooldown(1, 60.0)  # prevent over-usage
 async def on_message(ctx, member: discord.Member):
-    channel = bot.get_channel(843852696254808094)  # <-- hell
-    await member.move_to(channel)
-    await ctx.send(f"User {member} has been sent to hell")
+    channel: int = int(os.getenv('BANISH'))
+    move_to = bot.get_channel(channel)
+    await member.move_to(move_to)
+    await ctx.send(f"User {member} has been sent to {bot.get_channel(move_to)}!")
 
 
-# sends 2 users to hell (Specifically Amir and Tenzin){might make it more interesting later}
-@bot.command(name="banish2")
-@commands.cooldown(1, 60.0)
-async def on_message(ctx, member: discord.Member, member2: discord.Member):
-    channel = bot.get_channel(843852696254808094)  # <-- hell
-    await member.move_to(channel)
-    await member2.move_to(channel)
-    await ctx.send(f"User {member} and {member2} has been sent to hell")
-
-
-# Mute a user just cause ya can
+# Mute a user from voice chat
 @bot.command(name="mute")
 @commands.cooldown(1, 60.0)
 async def on_message(ctx, member: discord.Member):
     await member.edit(mute=True)
 
 
-# Unmute member
+# Unmute a user from voice chat
 @bot.command(name="unmute")
 async def on_message(ctx, member: discord.Member):
     await member.edit(mute=False)
 
-
-# Delete every message a user sends (Deletion Portion)
+# Helper function for 'silence' command
+# Delete every message a user sends 
 # Default silence time is 10 seconds
-async def murder(member, sil_time=10):
+async def delete_message(member, time_silenced=10):
     start = time.time()
 
     @bot.event
     async def on_message(ctx):
         end = time.time()
-        if end > (start + float(sil_time)):
+        if end > (start + float(time_silenced)):
             pass
         elif ctx.author.id == member:
             await ctx.delete()
 
 
-# Delete every message a user sends (Telling bot which user to delete messages)
+# Receives member and time input,
+# Identifies which user to delete messages from
 @bot.command(name="silence")
-async def on_message(ctx, member: discord.Member, gib_time):
+async def on_message(ctx, member: discord.Member, given_time):
     # .Member does not work on .author
 
+    # Ensure that the bot cannot silence itself by hardcoding its ID
     if member.id != 839691704000446464:
-        if float(gib_time) <= 600.0:
-            silenced_time = gib_time
+        # Silence time must be less than 5 minutes
+        # If a time beyond 5 minutes is given, the user that called the command is silenced by that time instead
+        if float(given_time) <= 300.0:
+            silenced_time = given_time
             silenced_member = member.id
-            await ctx.send(f'{ctx.member} has been silenced for {gib_time} seconds!')
-            await murder(silenced_member, silenced_time)
-        elif float(gib_time) > 600.0:
-            silenced_time = gib_time
+            await ctx.send(f'{ctx.member} has been silenced for {given_time} seconds!')
+            await delete_message(silenced_member, silenced_time)
+        elif float(given_time) > 300.0:
+            silenced_time = given_time
             silenced_member = ctx.author.id
-            await ctx.send(f'you fucked up {ctx.author.name}')
-            await ctx.send(f'{ctx.author.name} has been silenced for {gib_time} seconds!')
-            await murder(silenced_member, silenced_time)
+            await ctx.send(f'you messed up {ctx.author.name}')
+            await ctx.send(f'{ctx.author.name} has been silenced for {given_time} seconds!')
+            await delete_message(silenced_member, silenced_time)
+
+    # Punish users by silencing the user that tried to silece the bot
     else:
-        silenced_time = gib_time
+        silenced_time = given_time
         silenced_member = ctx.author.id
         await ctx.send(f'YOU CANT SILENCE GOD, PEASANT')
         await ctx.send(f'{ctx.author} has been silenced!')
-        await murder(silenced_member, silenced_time)
+        await delete_message(silenced_member, silenced_time)
 
 
 bot.run(TOKEN)
